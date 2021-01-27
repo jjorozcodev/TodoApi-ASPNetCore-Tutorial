f2ea78dc46c5745eff6f23d4ad27822d84a2f188
JJ Orozco López <jjorozcodev@gmail.com>
2021-01-27 12:58:38 -0600

    Create a web project 2.1
    Using TimeController to return the timespan

---
+++ TodoApi/TodoApi21/Controllers/TimeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TodoApi21.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
